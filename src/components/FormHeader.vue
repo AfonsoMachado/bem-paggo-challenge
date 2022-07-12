e4d8295fda6669d8219c4095032183bea4db253b
Afonso Machado <afonso.machado@blockhub.builders>
2022-07-12 01:28:33 -0300

    styling header

--- src/components/FormHeader.vue
+++ src/components/FormHeader.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="form-header">
-    <span class="form-title">
-      <slot name="title"></slot>
-    </span>
-    <span class="form-subtitle">
-      <slot name="subtitle"></slot>
-    </span>
+    <div class="form-header-text">
+      <span class="form-title">
+        <slot name="title"></slot>
+      </span>
+      <br />
+      <span class="form-subtitle">
+        <slot name="subtitle"></slot>
+      </span>
+    </div>
+    <div class="form-header-img">
+      <img src="../assets/img/box.svg" alt="" />
+    </div>
   </div>
 </template>
 
@@ -19,21 +25,31 @@ export default class FormHeader extends Vue {}
 <style scoped>
 .form-header {
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
+
+  overflow: hidden;
 }
 
 .form-header {
   background-color: var(--main-indigo);
-  border-radius: 40px 40px 0 0;
+  border-radius: 40px 40px 60% 40%;
 
-  height: 150px;
+  height: 200px;
   padding: 0px 30px;
 
   color: var(--main-white);
   font-size: 24px;
 
   text-align: left;
+}
+
+.form-header-text {
+  align-self: center;
+}
+
+.form-header-img {
+  margin-top: 10px;
 }
 
 .form-title {
